a353f64ebb9e7d3a970a93c73abba56b3f91aae8
Alexandr Kalyan <alexandrkalyan009@gmail.com>
2025-01-10 20:40:49 +0200

    Refactor HorizontalListViewSelectionIndicatorBehavior and simplify selection indicator logic

--- Ecierge.Uno.Controls/Behaviors/HorizontalListViewSelectionIndicatorBehavior.cs
+++ Ecierge.Uno.Controls/Behaviors/HorizontalListViewSelectionIndicatorBehavior.cs
@@ -1,76 +1,71 @@
-using Microsoft.Xaml.Interactivity;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml;
-using System.Linq;
+namespace Ecierge.Uno.Controls.Behaviors;
+
 using CommunityToolkit.WinUI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.Xaml.Interactivity;
+using System.Linq;
 using Windows.UI;
 
-namespace Ecierge.Uno.Controls.Behaviors
+public class HorizontalListViewSelectionIndicatorBehavior : Behavior<ListView>
 {
-    public class HorizontalListViewSelectionIndicatorBehavior : Behavior<ListView>
+    private ListViewItem? previousSelectedItem;
+
+#if !HAS_UNO
+    protected override void OnAttached()
     {
-        private ListViewItem? previousSelectedItem;
-
-        protected override void OnAttached()
+        base.OnAttached();
+        if (AssociatedObject is not null)
         {
-            base.OnAttached();
-#if !HAS_UNO
-            if (AssociatedObject is not null)
-            {
-                AssociatedObject.SelectionChanged += OnListViewSelectionChanged;
-            }
+            AssociatedObject.SelectionChanged += OnListViewSelectionChanged;
+        }
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        if (AssociatedObject is not null)
+        {
+            AssociatedObject.SelectionChanged -= OnListViewSelectionChanged;
+        }
+    }
 #endif
-        }
 
-        protected override void OnDetaching()
+    private void OnListViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is ListView listView &&
+            listView.SelectedItem is var selectedItem &&
+            listView.ContainerFromItem(selectedItem) is ListViewItem selectedListViewItem)
         {
-            base.OnDetaching();
-#if !HAS_UNO
-            if (AssociatedObject is not null)
-            {
-                AssociatedObject.SelectionChanged -= OnListViewSelectionChanged;
-            }
-#endif
+            RemoveSelectionIndicator(previousSelectedItem);
+            SetSelectionIndicatorProperties(listView, e, selectedListViewItem);
+            previousSelectedItem = selectedListViewItem;
         }
+    }
 
-        private void OnListViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+    private void SetSelectionIndicatorProperties(ListView listView, SelectionChangedEventArgs e, ListViewItem selectedListViewItem)
+    {
+        if (selectedListViewItem.FindDescendant<ListViewItemPresenter>() is { } selectedListViewItemPresenter &&
+            selectedListViewItemPresenter.FindDescendants().OfType<Border>().LastOrDefault() is Border selectionIndicator)
         {
-            if (sender is ListView listView &&
-                listView.SelectedItem is var selectedItem &&
-                listView.ContainerFromItem(selectedItem) is ListViewItem selectedListViewItem)
-            {
-                RemoveSelectionIndicator(previousSelectedItem);
-
-                MemberListView_SelectionChanged(listView, e, selectedListViewItem);
-
-                previousSelectedItem = selectedListViewItem;
-            }
+            selectionIndicator.BorderBrush = (Brush)Application.Current.Resources["AccentFillColorDefaultBrush"];
+            selectionIndicator.BorderThickness = new Thickness(3);
+            selectionIndicator.HorizontalAlignment = HorizontalAlignment.Center;
+            selectionIndicator.VerticalAlignment = VerticalAlignment.Bottom;
+            selectionIndicator.Width = 64;
         }
+    }
 
-        private void MemberListView_SelectionChanged(ListView listView, SelectionChangedEventArgs e, ListViewItem selectedListViewItem)
+    private void RemoveSelectionIndicator(ListViewItem? previousSelectedItem)
+    {
+        if (previousSelectedItem is not null)
         {
-            if (selectedListViewItem.FindDescendant<ListViewItemPresenter>() is { } selectedListViewItemPresenter &&
-                selectedListViewItemPresenter.FindDescendants().OfType<Border>().LastOrDefault() is Border selectionIndicator)
+            if (previousSelectedItem.FindDescendant<ListViewItemPresenter>() is { } previousListViewItemPresenter &&
+                previousListViewItemPresenter.FindDescendants().OfType<Border>().LastOrDefault() is Border previousSelectionIndicator)
             {
-                selectionIndicator.Width = 64;
-                selectionIndicator.BorderBrush = new SolidColorBrush((Color)Application.Current.Resources["SystemAccentColorLight2"]);
-                selectionIndicator.BorderThickness = new Thickness(3);
-                selectionIndicator.HorizontalAlignment = HorizontalAlignment.Center;
-                selectionIndicator.VerticalAlignment = VerticalAlignment.Bottom;
-            }
-        }
-
-        private void RemoveSelectionIndicator(ListViewItem? previousSelectedItem)
-        {
-            if (previousSelectedItem is not null)
-            {
-                if (previousSelectedItem.FindDescendant<ListViewItemPresenter>() is { } previousListViewItemPresenter &&
-                    previousListViewItemPresenter.FindDescendants().OfType<Border>().LastOrDefault() is Border previousSelectionIndicator)
-                {
-                    previousSelectionIndicator.Width = 3;
-                    previousSelectionIndicator.BorderThickness = new Thickness(0, 0, 0, 0);
-                }
+                previousSelectionIndicator.BorderThickness = new Thickness(0);
+                previousSelectionIndicator.Width = 3;
             }
         }
     }
