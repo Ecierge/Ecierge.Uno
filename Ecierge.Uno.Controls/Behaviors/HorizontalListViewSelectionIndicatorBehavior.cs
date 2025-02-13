7606a3695d0857b198cf6515777605aa9b9e53d9
WithinUA <withinua@gmail.com>
2025-02-13 16:53:15 +0200

    Properties added to the HorizontalListViewSelectionIndicatorBehavior

--- Ecierge.Uno.Controls/Behaviors/HorizontalListViewSelectionIndicatorBehavior.cs
+++ Ecierge.Uno.Controls/Behaviors/HorizontalListViewSelectionIndicatorBehavior.cs
@@ -10,6 +10,90 @@ public class HorizontalListViewSelectionIndicatorBehavior : Behavior<ListView>
 {
     private ListViewItem? previousSelectedItem;
 
+    public static readonly DependencyProperty SelectorMarginProperty =
+        DependencyProperty.Register(
+            "SelectorMargin",
+            typeof(Thickness),
+            typeof(HorizontalListViewSelectionIndicatorBehavior),
+            new PropertyMetadata(new Thickness(0))
+        );
+
+    public Thickness Margin
+    {
+        get => (Thickness)GetValue(SelectorMarginProperty);
+        set => SetValue(SelectorMarginProperty, value);
+    }
+
+    public static readonly DependencyProperty SelectorWidthProperty =
+        DependencyProperty.Register(
+            "SelectorWidth",
+            typeof(double),
+            typeof(HorizontalListViewSelectionIndicatorBehavior),
+            new PropertyMetadata(64.0)
+        );
+
+    public double Width
+    {
+        get => (double)GetValue(SelectorWidthProperty);
+        set => SetValue(SelectorWidthProperty, value);
+    }
+
+    public static readonly DependencyProperty VerticalAlignmentProperty =
+        DependencyProperty.Register(
+            "SelectorVerticalAlignment",
+            typeof(VerticalAlignment),
+            typeof(HorizontalListViewSelectionIndicatorBehavior),
+            new PropertyMetadata(VerticalAlignment.Bottom)
+        );
+
+    public VerticalAlignment VerticalAlignment
+    {
+        get => (VerticalAlignment)GetValue(VerticalAlignmentProperty);
+        set => SetValue(VerticalAlignmentProperty, value);
+    }
+
+    public static readonly DependencyProperty HorizontalAlignmentProperty =
+        DependencyProperty.Register(
+            "SelectorHorizontalAlignment",
+            typeof(HorizontalAlignment),
+            typeof(HorizontalListViewSelectionIndicatorBehavior),
+            new PropertyMetadata(HorizontalAlignment.Center)
+        );
+
+    public HorizontalAlignment HorizontalAlignment
+    {
+        get => (HorizontalAlignment)GetValue(HorizontalAlignmentProperty);
+        set => SetValue(HorizontalAlignmentProperty, value);
+    }
+
+    public static readonly DependencyProperty BorderBrushProperty =
+        DependencyProperty.Register(
+            "SelectorBorderBrush",
+            typeof(Brush),
+            typeof(HorizontalListViewSelectionIndicatorBehavior),
+            new PropertyMetadata((Brush)Application.Current.Resources["AccentFillColorDefaultBrush"])
+        );
+
+    public Brush BorderBrush
+    {
+        get => (Brush)GetValue(BorderBrushProperty);
+        set => SetValue(BorderBrushProperty, value);
+    }
+
+    public static readonly DependencyProperty BorderThicknessProperty =
+        DependencyProperty.Register(
+            "SelectorBorderThickness",
+            typeof(Thickness),
+            typeof(HorizontalListViewSelectionIndicatorBehavior),
+            new PropertyMetadata(new Thickness(3))
+        );
+
+    public Thickness Thickness
+    {
+        get => (Thickness)GetValue(BorderThicknessProperty);
+        set => SetValue(BorderThicknessProperty, value);
+    }
+
 #if !HAS_UNO
     protected override void OnAttached()
     {
@@ -17,6 +101,10 @@ public class HorizontalListViewSelectionIndicatorBehavior : Behavior<ListView>
         if (AssociatedObject is not null)
         {
             AssociatedObject.SelectionChanged += OnListViewSelectionChanged;
+            if (AssociatedObject.SelectedIndex >= 0)
+            {
+                AssociatedObject.Loaded += OnListViewLoaded;
+            }
         }
     }
 
@@ -29,6 +117,13 @@ public class HorizontalListViewSelectionIndicatorBehavior : Behavior<ListView>
         }
     }
 #endif
+
+    private void OnListViewLoaded(object sender, RoutedEventArgs e)
+    {
+        AssociatedObject.Loaded -= OnListViewLoaded;
+        var args = new SelectionChangedEventArgs([], [AssociatedObject.SelectedItem]);
+        OnListViewSelectionChanged(sender, args);
+    }
 
     private void OnListViewSelectionChanged(object sender, SelectionChangedEventArgs e)
     {
@@ -47,11 +142,12 @@ public class HorizontalListViewSelectionIndicatorBehavior : Behavior<ListView>
         if (selectedListViewItem.FindDescendant<ListViewItemPresenter>() is { } selectedListViewItemPresenter &&
             selectedListViewItemPresenter.FindDescendants().OfType<Border>().LastOrDefault() is Border selectionIndicator)
         {
-            selectionIndicator.BorderBrush = (Brush)Application.Current.Resources["AccentFillColorDefaultBrush"];
-            selectionIndicator.BorderThickness = new Thickness(3);
-            selectionIndicator.HorizontalAlignment = HorizontalAlignment.Center;
-            selectionIndicator.VerticalAlignment = VerticalAlignment.Bottom;
-            selectionIndicator.Width = 64;
+            selectionIndicator.BorderBrush = BorderBrush;
+            selectionIndicator.BorderThickness = Thickness;
+            selectionIndicator.HorizontalAlignment = HorizontalAlignment;
+            selectionIndicator.VerticalAlignment = VerticalAlignment;
+            selectionIndicator.Width = Width;
+            selectionIndicator.Margin = Margin;
         }
     }
 
