bc24de87c48afddc23a1def82b2af14885ab6ed2
Elena Alekseeva <aleklenoch@gmail.com>
2025-10-21 21:14:28 +0300

    Implemented PageContentControl

--- Ecierge.Uno.App/Presentation/MainViewModel.cs
+++ Ecierge.Uno.App/Presentation/MainViewModel.cs
@@ -28,6 +28,7 @@ public partial class MainViewModel : ObservableObject
         Title += $" - {localizer["ApplicationName"]}";
         Title += $" - {appInfo?.Value?.Environment}";
         GoToSecond = new AsyncRelayCommand(GoToSecondView);
+        GoToThird = new AsyncRelayCommand(GoToThirdView);
 
         var groups = new List<(string, string)>();
         for(int i = 0; i < 5; i++)
@@ -48,11 +49,17 @@ public partial class MainViewModel : ObservableObject
     public string? Title { get; }
 
     public ICommand GoToSecond { get; }
+    public ICommand GoToThird { get; }
 
     private async Task GoToSecondView()
     {
         if (string.IsNullOrWhiteSpace(Name)) return;
         await navigator.NavigateLocalSegmentAsync(this, "Second", new Entity(Name));
+    }
+    private async Task GoToThirdView()
+    {
+        if (string.IsNullOrWhiteSpace(Name)) return;
+        await navigator.NavigateLocalSegmentAsync(this, "Third", new Entity(Name));
     }
 
 }
---
+++ Ecierge.Uno.App/Presentation/ThirdPage.xaml.cs
@@ -0,0 +1,12 @@
+namespace Ecierge.Uno.App.Presentation;
+
+using Microsoft.UI.Xaml.Controls;
+
+public sealed partial class ThirdPage : Page
+{
+    public ThirdPage()
+    {
+        this.InitializeComponent();
+    }
+}
+
