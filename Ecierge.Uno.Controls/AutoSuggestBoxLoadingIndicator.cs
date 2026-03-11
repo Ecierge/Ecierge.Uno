e9184df3665b32c481155cd459c09ed19c3d089e
Iryna Konovalova <Iryna.Konovalova@eciergeos.com>
2026-03-11 21:27:14 +0100

    PR fixes

--- Ecierge.Uno.Controls/AutoSuggestBoxLoadingIndicator.cs
+++ Ecierge.Uno.Controls/AutoSuggestBoxLoadingIndicator.cs
@@ -1,34 +1,35 @@
 namespace Ecierge.Uno.Controls;
 
 using Microsoft.UI.Xaml;
+using CommunityToolkit.WinUI;
 
 public static class AutoSuggestBoxLoadingIndicator
 {
-    public static readonly DependencyProperty IsLoadingProperty = DependencyProperty.RegisterAttached(
+    public static readonly DependencyProperty IsActiveProperty = DependencyProperty.RegisterAttached(
         "IsActive",
         typeof(bool),
-        typeof(LoadingAutoSuggestBoxHelper),
-        new PropertyMetadata(false, OnIsLoadingChanged));
+        typeof(AutoSuggestBoxLoadingIndicator),
+        new PropertyMetadata(false, OnIsActiveChanged));
 
-    public static void SetIsLoading(DependencyObject obj, bool value)
+    public static void SetIsActive(DependencyObject obj, bool value)
     {
         if (obj is null)
             throw new ArgumentNullException(nameof(obj));
-        obj.SetValue(IsLoadingProperty, value);
+        obj.SetValue(IsActiveProperty, value);
     }
 
-    public static bool GetIsLoading(DependencyObject obj)
+    public static bool GetIsActive(DependencyObject obj)
     {
         if (obj is null)
             throw new ArgumentNullException(nameof(obj));
-        return (bool)obj.GetValue(IsLoadingProperty);
+        return (bool)obj.GetValue(IsActiveProperty);
     }
 
     private static readonly DependencyProperty CachedRingProperty =
     DependencyProperty.RegisterAttached(
         "_ProgressRing_",
         typeof(ProgressRing),
-        typeof(LoadingAutoSuggestBoxHelper),
+        typeof(AutoSuggestBoxLoadingIndicator),
         new PropertyMetadata(null));
 
     private static void SetCachedRing(DependencyObject obj, ProgressRing value)
@@ -41,7 +42,7 @@ public static class AutoSuggestBoxLoadingIndicator
         return (ProgressRing)obj.GetValue(CachedRingProperty);
     }
 
-    private static void OnIsLoadingChanged(
+    private static void OnIsActiveChanged(
     DependencyObject d,
     DependencyPropertyChangedEventArgs e)
     {
@@ -51,35 +52,18 @@ public static class AutoSuggestBoxLoadingIndicator
 
             if (ring is null)
             {
-                ring = FindDescendant<ProgressRing>(box);
+                ring = box.FindDescendant<ProgressRing>();
 
                 if (ring is not null)
                     SetCachedRing(box, ring);
             }
 
             if (ring is not null)
-                SetIsLoading(ring, (bool)e.NewValue);
+                SetIsActive(ring, (bool)e.NewValue);
         }
         else if (d is ProgressRing ring)
         {
             ring.IsActive = (bool)e.NewValue;
         }
-    }
-    private static T FindDescendant<T>(DependencyObject parent)
-    where T : DependencyObject
-    {
-        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
-        {
-            var child = VisualTreeHelper.GetChild(parent, i);
-
-            if (child is T result)
-                return result;
-
-            var descendant = FindDescendant<T>(child);
-            if (descendant != null)
-                return descendant;
-        }
-
-        return default;
     }
 }
